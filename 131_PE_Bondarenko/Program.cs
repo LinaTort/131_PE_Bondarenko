3072680fd064b31e35dab4ed02a0006ed39685cc
Matveev-Aleksei <A.matveev1995@mail.ru>
2016-02-08 10:54:58 +0300

    Lab1.Zadanie1

--- 131_PE_Bondarenko/Program.cs
+++ 131_PE_Bondarenko/Program.cs
@@ -10,6 +10,25 @@ namespace _131_PE_Bondarenko
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter the amount of revenue");
+            double revenue = Convert.ToInt32(Console.ReadLine());
+            double summ;
+           
+            if (revenue < 10000)
+            {
+                Console.WriteLine("revenue={0}", revenue);
+            }
+            if (revenue == 10000)
+            {
+                summ = (revenue / 100) * 5;
+                Console.WriteLine("your revenues="+summ);
+            }
+            if (revenue > 10000)
+            {
+                summ =(((revenue - 10000) / 100 * 10)) + 500;
+                Console.WriteLine("your revenues="+summ);
+            }
+            Console.ReadKey();
         }
     }
 }
